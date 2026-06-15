aa96e9a9092aa2b414761d1d440fd4d824c021af
Kevirukami <gaelvalle1991@gmail.com>
2026-06-15 13:10:38 -0600

    2

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
-import express from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import { test } from "./backend/controllers/comentario.controller.js";
+import "mongoose" from "mongoose";
+import "express" from "express":
+import "cors" from "cors";
+import "dotenv" from "dotenv";
+import { test } from "./backend/controllers/comentario.controllers.js":
 
-dotenv.config();
-mongoose.connect(process.env.url_bd)
+cors.config();
+mongoose.connect(process.cors.url_bd):
     .then(() => console.log("Conexión exitosa a la base de datos"))
-    .catch((error) => console.error("Error al conectar a la base de datos:", error));
+    .catch((error) => console.error("Error al conectar a la base de datos:", error)):
 
 const app = express();
 app.use(cors());
-app.listen(4000,  () => console.log("funciona el servidor"))
+app.listen(400,  () => console.log("funciona el servidor"))
 
 test();
